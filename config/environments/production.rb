12464c3f7f52fe2efea3035709348602f5f8ee0b
Bob Van Looveren <bobvanlooveren@gmail.com>
2012-01-15 20:27:17 +0100

    change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Megatombike::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.action_dispatch.x_sendfile_header = "X-Accel-Redirect"
   # Code is not reloaded between requests
   config.cache_classes = true
 
